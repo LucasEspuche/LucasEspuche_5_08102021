85e5dac97336748acd922e56cb2ccac6e4ecd9df
LucasEspuche <88545512+LucasEspuche@users.noreply.github.com>
2021-11-11 20:22:43 +0100

    update to cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
-(function displayCart() {
-    let storageStatus = JSON.parse(localStorage.getItem("product"));
-    if (storageStatus === null) {
+let storageStatus = JSON.parse(localStorage.getItem("product"));
+
+(function cartCheck() {
+    if (storageStatus === null || storageStatus == 0) {
         document.querySelector("#cart__items").innerHTML += `
             <article class="cart__item">
                 <h2>Aucun produit</h2>   
@@ -9,36 +10,100 @@
             </article>`;
     }
     else {
-        storageStatus.forEach(product => {
-            fetch(`http://localhost:3000/api/products/${product.id}`)
-                .then(function (res) {
-                    return res.json();
-                })
-                .then(function (item) {
-                    document.querySelector("#cart__items").innerHTML += `
-                    <article class="cart__item" data-id="{${product.id}}">
-                        <div class="cart__item__img">
-                            <img src="${item.imageUrl}" alt="${item.altTxt}">
-                        </div>
-                        <div class="cart__item__content">
-                            <div class="cart__item__content__titlePrice">
-                                <h2>${item.name}</h2>
-                                <p>${product.color}</p>
-                                <p>${item.price * product.quantity} €</p>
-                            </div>
-                            <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                    <p>Qté : </p>
-                                    <input type="number" class="itemQuantity" name="itemQuantity"
-                                        min="1" max="100" value="${product.quantity}">
-                                </div>
-                                <div class="cart__item__content__settings__delete">
-                                    <p class="deleteItem">Supprimer</p>
-                                </div>
-                            </div>
-                        </div>
-                    </article>`;
-                });
-        });
+        cartDisplay();
     }
 })();
+
+async function cartDisplay() {
+    storageStatus.forEach(async product => {
+        await fetch(`http://localhost:3000/api/products/${product.id}`)
+            .then(res => res.json())
+            .then(item => {
+                document.querySelector("#cart__items").innerHTML += `
+                <article class="cart__item" data-id="{${product.id}}">
+                    <div class="cart__item__img">
+                        <img src="${item.imageUrl}" alt="${item.altTxt}">
+                    </div>
+                    <div class="cart__item__content">
+                        <div class="cart__item__content__titlePrice">
+                            <h2>${item.name}</h2>
+                            <p>${product.color}</p>
+                            <p>${item.price * product.quantity} €</p>
+                        </div>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                                <p>Qté : </p>
+                                <input type="number" class="itemQuantity" name="itemQuantity"
+                                    min="1" max="100" value="${product.quantity}">
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                                <p class="deleteItem">Supprimer</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>`;
+            });
+        productQuantity();
+        productRemoval();
+        productsTotalAmount();
+        productsTotalPrice();
+    });
+}
+
+function productQuantity() {
+    let quantitySelectors = document.querySelectorAll(".itemQuantity");
+    for (let i = 0; i < quantitySelectors.length; i += 1) {
+        quantitySelectors[i].addEventListener("change", () => {
+            for (let j = 0; j < storageStatus.length; j += 1) {
+                if (storageStatus[i].id === storageStatus[j].id &&
+                    storageStatus[i].color === storageStatus[j].color) {
+                    storageStatus[j].quantity = quantitySelectors[j].value;
+                    localStorage.setItem("product", JSON.stringify(storageStatus));
+                    location.reload();
+                }
+            };
+        });
+    };
+}
+
+function productRemoval() {
+    let removeButtons = document.querySelectorAll(".deleteItem");
+    for (let k = 0; k < removeButtons.length; k += 1) {
+        removeButtons[k].addEventListener("click", () => {
+            for (let l = 0; l < storageStatus.length; l += 1) {
+                if (storageStatus[k].id === storageStatus[l].id &&
+                    storageStatus[k].color === storageStatus[l].color) {
+                    storageStatus.splice(l, 1);
+                    localStorage.setItem("product", JSON.stringify(storageStatus));
+                    location.reload();
+                }
+            };
+        });
+    };
+}
+
+function productsTotalAmount() {
+    let amountDisplay = document.querySelector("#totalQuantity");
+    let productsAmount = [];
+    storageStatus.forEach(product => {
+        let totalProducts = parseInt(product.quantity);
+        productsAmount.push(totalProducts);
+        let reducer = (accumulator, currentValue) => accumulator + currentValue;
+        amountDisplay.textContent = productsAmount.reduce(reducer, 0);
+    });
+}
+
+async function productsTotalPrice() {
+    let priceDisplay = document.querySelector("#totalPrice");
+    let productsPrice = [];
+    storageStatus.forEach(async product => {
+        await fetch(`http://localhost:3000/api/products/${product.id}`)
+            .then(res => res.json())
+            .then(item => {
+                let totalPrice = product.quantity * item.price;
+                productsPrice.push(totalPrice);
+                let reducer = (accumulator, currentValue) => accumulator + currentValue;
+                priceDisplay.textContent = productsPrice.reduce(reducer, 0);
+            });
+    });
+}
